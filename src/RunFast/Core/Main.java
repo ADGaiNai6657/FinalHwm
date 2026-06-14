af1181aa2a3333483ec354a6400072d8f1dd10f0
ADGaiNai6657 <h18707490829@gmail.com>
2026-06-14 23:45:29 +0800

    Add login interface

--- src/RunFast/Core/Main.java
+++ src/RunFast/Core/Main.java
@@ -1,7 +1,10 @@
 package RunFast.Core;
 
+import javax.swing.SwingUtilities;
+
 public class Main {
     public static void main(String[] args) {
-        new PockerGame();
+        //程序入口，启动登录界面
+        SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
     }
 }
